9dad0b198bc383e53b027efa6da2ddf1dec29501
ColdmaterL <lucasmenmor@hotmail.com>
2021-10-08 16:45:55 -0300

    Projeto Finalizado

---
+++ Products/Entities/UsedProduct.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Products.Entities
+{
+    class UsedProduct : Product
+    {
+        public DateTime ManufacturedDate { get; set; }
+    
+        public UsedProduct()
+        {
+        }
+
+        public UsedProduct(string name, double price, DateTime manufactureddate) : base(name, price)
+        {
+            ManufacturedDate = manufactureddate;
+        }
+
+        public sealed override string PriceTag()
+        {
+            return $"{Name} (used) $ {Price:F2} (Manufactured date: {ManufacturedDate.ToString("dd/MM/yyyy")})";
+        }
+    }
+}
---
+++ Products/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Products.Entities;
+
+namespace Products
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int ProductNumb, Product;
+            char TypeOfProduct;
+            string NameProduct;
+            double PriceProduct, FeeProduct;
+            DateTime DateOfProduct;
+            List<Product> ListProducts = new List<Product>();
+
+            Console.Write("Enter the number of products: ");
+            ProductNumb = int.Parse(Console.ReadLine());
+            for(Product = 1; Product <= ProductNumb; Product++)
+            {
+                Console.WriteLine($"Product #{Product} data:");
+                Console.Write("Commom, used or imported (c/u/i)? ");
+                TypeOfProduct = char.Parse(Console.ReadLine());
+                Console.Write("Name: ");
+                NameProduct = Console.ReadLine();
+                Console.Write("Price: ");
+                PriceProduct = double.Parse(Console.ReadLine());
+                switch (TypeOfProduct)
+                {
+                    case 'c':
+                        ListProducts.Add(new Product(NameProduct, PriceProduct));
+                        break;
+                    case 'u':
+                        Console.Write("Manufactured Date (DD/MM/YYYY): ");
+                        DateOfProduct = DateTime.Parse(Console.ReadLine());
+                        ListProducts.Add(new UsedProduct(NameProduct, PriceProduct, DateOfProduct));
+                        break;
+                    case 'i':
+                        Console.Write("Customs fee: ");
+                        FeeProduct = double.Parse(Console.ReadLine());
+                        ListProducts.Add(new ImportedProduct(NameProduct, PriceProduct, FeeProduct));
+                        break;
+                }
+            }
+
+            Console.WriteLine("\nPRICE TAGS:");
+            foreach(Product prod in ListProducts)
+            {
+                Console.WriteLine(prod.PriceTag());
+            }
+        }
+    }
+}
